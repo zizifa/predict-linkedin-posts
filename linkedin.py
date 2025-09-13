dd7363ad6b239aaec1de6448e59b18a2e22654a7
Zizi__fa <89692143+zizifa@users.noreply.github.com>
2025-09-14 01:44:11 +0430

    normalize text post and remove extra things

--- linkedin.py
+++ linkedin.py
@@ -1,3 +1,4 @@
+
 import pandas as pd
 import numpy as np
 from hazm import *
@@ -7,9 +8,27 @@ from keras.activations import relu
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.model_selection import train_test_split
 import json
+import sys
 
-
-
+data=[]
 with open('',encoding='utf-8') as f:
     post = json.load(f)
+
+for p in post :
+    temporarypost = p['post']
+    normal = Normalizer()
+    tokenize =WordTokenizer()
+    
+    
+    normaliztext=normal.normalize(temporarypost)
+    token =tokenize.tokenize(normaliztext)
+    
+    
+    cleantoken=[tk for tk in token if tk.isalnum()]
+    cleanpost = " ".join(cleantoken)
+    print(cleanpost)
+    
+    data.append({'post': cleanpost , 'reaction' : p['reactions'], 'comments' : p['comments']})
+    
+    
 
