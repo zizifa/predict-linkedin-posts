c0418bab54783fcd929f2efeb449b69ca67d6963
Zizi__fa <89692143+zizifa@users.noreply.github.com>
2025-09-14 01:14:15 +0430

    load data

--- linkedin.py
+++ linkedin.py
@@ -1,1 +1,15 @@
-#start
+import pandas as pd
+import numpy as np
+from hazm import *
+from keras.layers import Dense
+from keras.models import Sequential 
+from keras.activations import relu
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.model_selection import train_test_split
+import json
+
+
+
+with open('',encoding='utf-8') as f:
+    post = json.load(f)
+
